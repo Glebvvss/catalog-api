a48c498345499025f6f3c574482ad71add901036
glebvvss <glebvvss@ukr.net>
2021-10-09 15:37:31 +0300

    update setup scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
-docker-compose stop
+#!/usr/bin/env bash
+
+if test -f './.installed'; then
+  docker-compose up -d
+  echo "Environment is ready to use."
+  echo "Application can be accessed by http://localhost:9001"
+  exit 0
+fi
+
+echo "npm install..."
+$(cd ./src && npm ci)
+$(cd ../)
 
 docker-compose up -d
+
+echo "Standard password value: password"
+echo "Please wait..."
+sleep 15s
+
+docker exec -it catalog_mysql bash -c "cd /myapp/mysql && sh bootstrap.sh"
+
+touch .installed
+echo "Environment is ready to use."
+echo "Application can be accessed by http://localhost:9001"
